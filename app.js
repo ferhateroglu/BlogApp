01a49a7200b99f52afd7532977bf074b5361f101
Ferhat Eroglu <ferhateroglu1907@gmail.com>
2022-01-14 20:10:15 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const fn = function () {
-  console.log('denme');
-};
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.static('public'));
+
+app.get('/',(req,res) =>{
+    res.sendFile(path.resolve(__dirname,'temp/index.html'));
+})
+
+app.listen(port, ()=>{
+    console.log(`sunucu ${port} portunda başlatıldı`);
+})
